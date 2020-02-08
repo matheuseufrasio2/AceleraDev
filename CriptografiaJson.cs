4b27dfc1afb1decf4a9b5c8ff3441f86c33de078
Matheus Eufrásio <matheuseufrasio2@gmail.com>
2020-02-08 10:24:14 -0300

    Primeiro commit

---
+++ CriptografiaJson.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AceleraDev
+{
+    class CriptografiaJson
+    {
+        public int numero_casas { get; set; }
+        public string token { get; set; }
+        public string cifrado { get; set; }
+        public string decifrado { get; set; }
+        public string resumo_criptografico { get; set; }
+
+        public void GeraDecifradoJson(int numerodeCasas, string codificado)
+        {
+            try
+            {
+                char[] temp = codificado.ToCharArray();
+
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    int letra = (int)temp[i];
+                    if (letra >= 'a' && letra <= 'z')
+                    {
+                        if (letra <= 'j')
+                        {
+                            letra += 16;
+                        }
+                        else
+                        {
+                            letra -= 10;
+                        }
+                    }
+
+                    temp[i] = (char)letra;
+                }
+                decifrado = new string(temp);
+                Console.WriteLine($"Json completamente decifrado: {decifrado}");
+            }
+            catch (Exception x)
+            {
+                throw new Exception(x.Message);
+            }
+            
+        }
+
+
+        public void GeraResumoCriptografico(string decodificado)
+        {
+            try
+            {
+                byte[] buffer = Encoding.Default.GetBytes(decodificado);
+                SHA1CryptoServiceProvider cryptoTransformSHA1 = new SHA1CryptoServiceProvider();
+                string hash = BitConverter.ToString(cryptoTransformSHA1.ComputeHash(buffer)).Replace("-", "");
+                resumo_criptografico = hash;
+                Console.WriteLine($"Resumo da Criptografia feito: {resumo_criptografico}");
+            }
+            catch (Exception x)
+            {
+                throw new Exception(x.Message);
+            }
+        }
+
+        public void AtualizaJson(string path, string decifrado, string resumo)
+        {
+            try
+            {
+                string text = File.ReadAllText(path);
+                text = text.Replace("\"decifrado\":\"\"", $"\"decifrado\":\"{decifrado}\"");
+                text = text.Replace("\"resumo_criptografico\":\"\"", $"\"resumo_criptografico\":\"{resumo}\"");
+                File.WriteAllText(path, text);
+                Console.WriteLine("Arquivo atualizado com sucesso.");
+            }
+            catch (Exception x)
+            {
+                throw new Exception(x.Message);
+            }
+            
+        }
+
+        //public HttpResponseMessage EnviarArquivoParaAPI(string arquivo, string urlApi)
+        //{
+        //    try
+        //    {
+        //        byte[] fileBytes = File.ReadAllBytes(arquivo);
+
+        //        HttpContent bytesContent = new ByteArrayContent(fileBytes);
+
+        //        // Submit the form using HttpClient and 
+        //        // create form data as Multipart (enctype="multipart/form-data")
+
+        //        using (var client = new HttpClient())
+        //        using (var formData = new MultipartFormDataContent())
+        //        {
+        //            formData.Add(bytesContent, "answer", Path.GetFileName(arquivo));
+        //            HttpResponseMessage response = client.PostAsync(urlApi, formData).Result;
+
+        //            // equivalent of pressing the submit button on the form
+        //            if (!response.IsSuccessStatusCode)
+        //            {
+        //                return null;
+        //            }
+        //            return response.Content.ReadAsStreamAsync().Result;
+        //        }
+        //    }
+        //    catch (Exception x)
+        //    {
+        //        throw new Exception(x.Message);
+        //    }
+        //}
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace AceleraDev
+{
+    class Program
+    {
+        static string url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=2f495d80465d9040c4a2037999b6a321746e7fa4";
+        static string path = @"C:\Users\Matheus Eufrásio\source\repos\AceleraDev\ArquivosGerados/answer.json";
+        static string fraseJson;
+        static HttpClient httpClient = new HttpClient();
+        static void Main(string[] args)
+        {
+            fraseJson =  GetJson(url);
+            GerarArquivoJson(path, fraseJson);
+            CriptografiaJson criptografiaJson = JsonConvert.DeserializeObject<CriptografiaJson>(fraseJson);
+            criptografiaJson.GeraDecifradoJson(criptografiaJson.numero_casas, criptografiaJson.cifrado);
+            criptografiaJson.GeraResumoCriptografico(criptografiaJson.decifrado);
+            criptografiaJson.AtualizaJson(path, criptografiaJson.decifrado, criptografiaJson.resumo_criptografico);
+            //criptografiaJson.EnviarArquivoParaAPI();
+
+            var content = new MultipartFormDataContent();
+            httpClient.BaseAddress = new Uri(url);
+
+            var fileContent1 = new ByteArrayContent(File.ReadAllBytes(path));
+
+            fileContent1.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment") { 
+                FileName = "answer"
+            };
+
+            content.Add(fileContent1);
+
+            var result = httpClient.PostAsync("file/upload", content).Result;
+
+            Console.WriteLine("Status: " + result.StatusCode);
+
+
+            Console.ReadLine();
+        }
+
+
+
+        static string GetJson(string url)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    string response = client.DownloadString(url);
+                    if (!string.IsNullOrEmpty(response))
+                    {
+                        Console.WriteLine("Começando o Processo.");
+                    }
+                    Console.WriteLine("Request feito a API");
+                    return response;
+                }
+            }
+            catch (Exception x)
+            {
+                throw new Exception(x.Message);
+            }
+            
+
+
+        }
+
+        private static void GerarArquivoJson(string path, string fraseJson)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    File.WriteAllText(path, fraseJson);
+                    Console.WriteLine("Aquivo criado com sucesso.");
+                }
+                else
+                {
+                    Console.WriteLine("Já existe um arquivo no diretório.");
+                }
+            }
+            catch (Exception x)
+            {
+                throw new Exception(x.Message);
+            }
+            
+        }
+    }
+}
+
+
+//{
+//    "numero_casas":10,
+//    "token":"2f495d80465d9040c4a2037999b6a321746e7fa4",
+//    "cifrado":"nyx xyd wkuo dro ecob zbyfsno sxpybwkdsyx drkd dro cicdow kvbokni uxygc. bsmu vowyxc",
+//    "decifrado":"",
+//    "resumo_criptografico":""
+//}
